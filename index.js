58aef6fce2d7f0f571836a7ebdd0ac76d9688500
Ernesto Garcia <ernestogarcia@Ernestos-MacBook-Air.local>
2024-02-18 23:43:02 -0500

    completed exercise for Backedn Cert. at freecodecamp.

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+import dotenv from "dotenv";
+dotenv.config();
 
-var app = express();
+import express from "express";
+import cors from "cors";
+import multer from "multer";
+
+const app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
-
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use("/public", express.static(process.cwd() + "/public"));
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
+// Set up Multer storage
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
 
+// Initialize Multer with storage configuration
+const upload = multer({ storage: storage });
 
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  // Access the file metadata from req.file
+  const metadata = {
+    name: req.file.originalname, // File name
+    type: req.file.mimetype, // File type (MIME type)
+    size: req.file.size, // File size in bytes
+  };
+
+  res.json(metadata);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
